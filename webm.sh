fbdd7bc1c7d48b8c47185eaada02a7a10f5789fc
gitbuddy <webmaster@seeder.us>
2012-09-20 13:36:34 +0200

    Update webm.sh
    added qt-faststart to enable mmp4 streaming
    
    remove original files, remove none compatible stream mp4

--- webm.sh
+++ webm.sh
@@ -3,6 +3,9 @@ DIR=/var/www/tmpfiles
 find $DIR -type f |
 while read i
 do
-fmpeg -i "$i" -vcodec libx264 -fpre "/usr/local/bin/libx264-ipod320.ffpreset" -crf 20 -threads 0 "$i".wmp4 2> /var/www/logs/progress.txt
-done
+ffmpeg -i "$i" -vcodec libx264 -fpre "/usr/local/bin/libx264-ipod320.ffpreset" -crf 20 -threads 0 "$i".mp4 2> /var/www/logs/progress.txt
 
+sleep 5
+
+qt-faststart "$i".mp4 "$i".qt.mp4 && rm "$i" && "$i".mp4
+done

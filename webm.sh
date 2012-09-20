092b48f0896e28bd3404f240fc2a3c32f2d02b6f
gitbuddy <webmaster@seeder.us>
2012-09-20 12:46:27 +0200

    changed to libx264 MP4
    # i worked with this version http://dl.dropbox.com/u/24633983/ffmpeg/index.html ubuntu 10.4 64bit
    # you just need to unzip the static ffmpeg into /usr/local/bin/ and the preset files next to ffmpeg ok

--- webm.sh
+++ webm.sh
@@ -3,6 +3,6 @@ DIR=/var/www/tmpfiles
 find $DIR -type f |
 while read i
 do
-ffmpeg -i "$i"  "$i".webm 2> /var/www/logs/progress.txt
+fmpeg -i "$i" -vcodec libx264 -fpre "/usr/local/bin/libx264-ipod320.ffpreset" -crf 20 -threads 0 "$i".wmp4 2> /var/www/logs/progress.txt
 done
 
